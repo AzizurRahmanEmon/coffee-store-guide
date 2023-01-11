5ff512b6eff6e6c1bbb9931570f6bce6ec674720
Azizur Rahman <aremon2533@gmail.com>
2023-01-11 23:44:49 +0600

    11-01

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,18 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner'
 import Card from '../components/card'
-import coffeeStores from '../data/coffee-stores.json'
+import coffeeStoresData from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context){
+  return{
+    props:{
+      coffeeStores:coffeeStoresData,
+    }
+  }
+}
+
+export default function Home(props) {
+  console.log(props,"props");
   const handleOnBannerBtnClick=()=>{
     console.log('hi banner button');
   }
@@ -24,16 +33,23 @@ export default function Home() {
         <div className={styles.heroImage}>
         <Image src="/static/hero-image.png" width={700} height={400}/>
         </div>
+        {props.coffeeStores.length>0 && (
+        <>
+        <h2 className={styles.heading2}>Coffee Stores</h2>
         <div className={styles.cardLayout}>
-          {coffeeStores.map(coffeeStore=>{
-            return <Card 
+        {props.coffeeStores.map(coffeeStore=>{
+        return (
+        <Card 
+        key={coffeeStore.id}
         name={coffeeStore.name}
         imgUrl={coffeeStore.imgUrl}
         href={`/coffee-store/${coffeeStore.id}`}
-        className={styles.card}
         />
+        )
           })}
         </div>
+        </>
+        )}
       </main> 
       </div>
     </>
